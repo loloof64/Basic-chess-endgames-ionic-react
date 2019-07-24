5a000f05675a6171739fce786a9091c1615962d5
Laurent Bernabe <laurent.bernabe@gmail.com>
2019-07-24 23:22:33 +0200

    Board is centered on the page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,38 @@
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonHeader, IonTitle, IonToolbar} from '@ionic/react';
+import React, {Component} from 'react';
 
 import Chessboard from 'chessboardjsx';
 
-const Home: React.FunctionComponent = () => {
-  return (
-    <>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <Chessboard position="start" />
-      </IonContent>
-    </>
-  );
+interface ScreenDimensions {
+  screenWidth: number;
+  screenHeight: number;
+};
+
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Ionic Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-no-padding">
+          <Chessboard 
+            position="start" 
+            calcWidth={this.calculateSize}
+            boardStyle={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}
+          ></Chessboard>
+        </IonContent>
+      </>
+    );
+  }
+
+  private calculateSize(newDimensions: ScreenDimensions) {
+    return newDimensions.screenWidth < newDimensions.screenHeight ?
+      newDimensions.screenWidth : newDimensions.screenHeight - 60;
+  }
+  
 };
 
 export default Home;
